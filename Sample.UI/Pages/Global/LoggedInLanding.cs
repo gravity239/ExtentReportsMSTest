bd33bebae59b9f173c892dae2037d6cbe2ecc695
gravity239 <gravity239@gmail.com>
2019-11-14 11:45:56 +0700

    remove redundant

--- Sample.UI/Pages/Global/LoggedInLanding.cs
+++ Sample.UI/Pages/Global/LoggedInLanding.cs
@@ -10,26 +10,6 @@ namespace Sample.UI.Pages.Global
     {
 
         #region Entities
-
-        private static By _userNameLable => By.XPath("//div[@id='divUserName']/span");
-        private static By _logoutLink => By.Id("LogoutLabel");
-        private static By _logoutYesButton => By.XPath("//div[@class='rwDialogPopup radconfirm']//a[.//span[text()='Yes']]");
-        private static By _alertPopup => By.Id("kendoAlertWindow");
-        private static By _saveChangeYesButton => By.Id("Yes");
-        private static By _saveChangeNoButton => By.Id("No");
-        private static By _saveItemPopUp => By.XPath("//div[contains(@id,'RadWindowWrapper_alert')]");
-        private static By _iframeAutoRecovery => By.XPath("//iframe[@name='RadWindowAutoRecovery']");
-        private static By _recoveryCancelButton => By.Id("divCancel");
-        private static By _recoveryNoButton => By.Id("divNo");
-
-
-        public IWebElement LogoutLink { get { return StableFindElement(_logoutLink); } }
-        public IWebElement LogoutYesButton { get { return StableFindElement(_logoutYesButton); } }
-        public IWebElement SaveChangeYesButton { get { return StableFindElement(_saveChangeYesButton); } }
-        public IWebElement SaveChangeNoButton { get { return StableFindElement(_saveChangeNoButton); } }
-        public IWebElement IframeAutoRecovery { get { return StableFindElement(_iframeAutoRecovery); } }
-        public IWebElement RecoveryCancelButton { get { return StableFindElement(_recoveryCancelButton); } }
-        public IWebElement RecoveryNoButton { get { return StableFindElement(_recoveryNoButton); } }
         #endregion
 
 
