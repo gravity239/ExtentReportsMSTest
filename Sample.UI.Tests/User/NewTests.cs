8446a29b82c8c9b774552365660b83e004863502
gravity239 <gravity239@gmail.com>
2020-02-06 16:07:41 +0700

    add new test to check parallel

--- Sample.UI.Tests/User/NewTests.cs
+++ Sample.UI.Tests/User/NewTests.cs
@@ -1,44 +1,24 @@
-﻿using FluentAssertions;
-using Sample.Common.Helper;
-using Sample.UI.Pages;
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using static Sample.Common.Helper.ExtentReportsHelper;
-using Sample.UI.Utilities;
 using Sample.UI.Pages.Google;
+using FluentAssertions;
+using Sample.UI.Utilities;
 
 namespace Sample.UI.Tests.User
 {
+    /// <summary>
+    /// Summary description for NewTests
+    /// </summary>
     [TestClass]
-    public class NewGoogleTests : Test
+    public class NewTests : Test
     {
         Home google = PageFactory.Get<Home>();
 
-        [TestCategory("HappyPath")]
         [SampleTestMethod]
-        public void TC003()
-        {
-            //Given
-            //1. Navigate to Google Home Page.
-            test.Info("Navigate to Google page.");
-            NavigateToUrl("http://google.com");
-
-            //When
-            //2. Enter search text.
-            test = LogTest("TC003 - Verify that search value is correct.");
-            //var google = PageFactory.Get<Home>();
-            google.Search("abc");
-
-            //Then
-            //VP: Verify that search text displays correctly 
-            validations.Add(google.ValidateSearchValue("abc"));
-            Console.WriteLine(string.Join(System.Environment.NewLine, validations.ToArray()));
-            validations.Should().OnlyContain(validations => validations.Value).Equals(bool.TrueString);
-        }
-
-
-        [SampleTestMethod]
-        public void TC004()
+        public void TC006()
         {
             //Given
             //1. Navigate to Google Home Page.
@@ -58,9 +38,8 @@ namespace Sample.UI.Tests.User
             validations.Should().OnlyContain(validations => validations.Value).Equals(bool.TrueString);
         }
 
-
         [SampleTestMethod]
-        public void TC005()
+        public void TC007()
         {
             //Given
             //1. Navigate to Google Home Page.
@@ -81,4 +60,3 @@ namespace Sample.UI.Tests.User
         }
     }
 }
-
